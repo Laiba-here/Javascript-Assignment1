4d3d64e16d1775d6532f454736902c499d37e805
Laiba-here <nazlaiba230@gmail.com>
2024-09-27 21:39:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+//        .... QUESTION 1 ....
+// Greet website visitor script
+
+// Get the visitor's name (optional)
+// var visitorName = prompt("What is your name?");
+
+// Greet the visitor
+// if (visitorName != null && visitorName != "") {
+//   alert("Assalam o alaikum, " + visitorName + "! Welcome to our website.");
+// } else {
+//   alert("Assalam o alaikum! Welcome to our website.");
+// }
+
+
+//         .... QUESTION 2 ....
+
+// function checkPassword(){ 
+//     let password = document.getElementById("password").value;
+//     let cnfrmpassword = document.getElementById("cnfrm-password").value;
+//     console.log(password,cnfrmpassword);
+//     // let message =document.getElementById("message");
+
+//         if(password == cnfrmpassword){
+//             alert("Correct password")
+//         }
+//         else{
+//             alert("Enter a valid password")
+//         }
+//     }
+
+
+//          .... QUESTION 3 ....
+// alert("Welcome to JS Land....\nHappy coding");
+
+// Display the message on the webpage
+// document.write("Welcome to JS Land....<br>Happy coding");
+
+//         .... QUESTION 4 ....
+// alert("Welcome to JS Land...")
+
+// alert("Happy Coading!")
+
+
+//        .... QUESTION 5 ....
+// console.log("Hello... I can run JS through my web browser's console")
+
+//        .... QUESTION 6 .....
+// document.getElementById('alertButton').addEventListener('click', function() {
+    // alert("Hello! Welcome to my project!");
+// });
+
+//            ....QUESTION 7 ....
